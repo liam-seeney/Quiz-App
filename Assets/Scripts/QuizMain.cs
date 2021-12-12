7116b49a737e47a3a270078c111baf82e5da898e
Liam <lms.seeney@gmail.com>
2021-12-12 18:07:25 +0000

    Change question
    - changes question after answer is selected

--- Assets/Scripts/QuizMain.cs
+++ Assets/Scripts/QuizMain.cs
@@ -11,18 +11,26 @@ public class QuizMain : MonoBehaviour
     [SerializeField] private List<GameObject> answerButtons;
 
     private Question question;
+    private Color selectedColor = new Color32(54, 255, 45, 255);
+    private Color defaultColor = new Color32(255, 255, 255, 255);
 
     private void Start()
     {
-        question = questions[Random.Range(0, questions.Count)];
-
+        GetRandomIndex();
         SetQuestionText();
-        SetAnswerButtons();
+    }
+
+    private void GetRandomIndex()
+    {
+        int randomIndex = Random.Range(0, questions.Count);
+        question = questions[randomIndex];
+        questions.RemoveAt(randomIndex);
     }
 
     private void SetQuestionText()
     {
         questionTextArea.text = question.GetQuestionText();
+        SetAnswerButtons();
     }
 
     private void SetAnswerButtons()
@@ -37,22 +45,53 @@ public class QuizMain : MonoBehaviour
     public void AnswerSelected(int index)
     {
         SetButtonState(false);
+
         if (index == question.GetCorrectAnswerIndex())
         {
             questionTextArea.text = "Correct!";
+            SetButtonColour(index);
         }
         else
         {
-            questionTextArea.text = $"Wrong! The correct answer is:\n {question.GetAnswers(question.GetCorrectAnswerIndex())}";
+            questionTextArea.text = $"Wrong!\n The correct answer is:\n {question.GetAnswers(question.GetCorrectAnswerIndex())}";
+            SetButtonColour(question.GetCorrectAnswerIndex());
         }
+
+        StartCoroutine(LoadNextQuestion());
+    }
+
+    private void SetButtonColour(int index)
+    {
+        Image button = answerButtons[index].GetComponent<Image>();
+        button.color = selectedColor;
     }
 
     private void SetButtonState(bool state)
     {
         for (int i = 0; i < answerButtons.Count; i++)
         {
-            Button button = answerButtons[i].GetComponent<Button>();
-            button.interactable = state;
+            {
+                Button button = answerButtons[i].GetComponent<Button>();
+                button.interactable = state;
+            }
+        }
+    }
+
+    private IEnumerator LoadNextQuestion()
+    {
+        yield return new WaitForSeconds(2f);
+        SetButtonState(true);
+        SetDefaultButtonColors();
+        GetRandomIndex();
+        SetQuestionText();
+    }
+
+    private void SetDefaultButtonColors()
+    {
+        for (int i = 0; i < answerButtons.Count; i++)
+        {
+            Image buttonImage = answerButtons[i].GetComponent<Image>();
+            buttonImage.color = defaultColor;
         }
     }
 }
